23649feeac48d59162dcb31c603247950691323a
Yash Salunke <yash3a@gmail.com>
2020-07-08 10:52:22 +0530

    Web Build Added

---
+++ web-build/precache-manifest.fa3d07c867d7c007791510877a78ff92.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae1c6927e8b5aad2c049",
+    "url": "/static/js/app.340f5bf2.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "fd0b91aa475b52381214",
+    "url": "/static/js/2.d7bb5818.chunk.js"
+  },
+  {
+    "revision": "864b5a2a12e395a0aba167209a78df95",
+    "url": "/static/media/rock.79436ec1.png"
+  },
+  {
+    "revision": "cffb3100c0baea623b0353f9e933ef62",
+    "url": "/static/media/paper.599c25df.png"
+  },
+  {
+    "revision": "48d238a67d2b41996cd45194a93b7b10",
+    "url": "/static/media/scissors.f27b7752.png"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "ef21d3386d8047b114f58d71b77bd963",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4197a9fd61e635cea7da7906870ef364",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/static/js/2.d7bb5818.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "606ad291d5a634fb79b339b1a2f7bdb6",
+    "url": "/manifest.json"
+  }
+];
